7a2d16ad2281677f481bc38a513d6720883c96f5
zdm <zdm@softvisio.net>
2018-08-28 00:55:02 +0300

    release v0.2.0

--- lib/common/header.pm
+++ lib/common/header.pm
@@ -1,4 +1,4 @@
-package common::header v0.1.6;
+package common::header v0.2.0;
 
 # NOTE !!!WARNING!!! don't use indirect with strawberry perl
 # https://rt.cpan.org/Public/Bug/Display.html?id=102321
